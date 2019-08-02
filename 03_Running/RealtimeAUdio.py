18cbd2107c95ddd66218c272b2561485a14f557d
David Goedicke <da.goedicke@gmail.com>
2019-08-02 02:09:23 -0400

    only retraining the last layer

--- 03_Running/RealtimeAUdio.py
+++ 03_Running/RealtimeAUdio.py
@@ -15,24 +15,28 @@ from  numpy_ringbuffer import RingBuffer
 from torch.autograd import Variable
 from threading import Thread
 from time import sleep
-import cv2 as cv
+import cv2
 import time
+import pickle
 
 
 print("ExecutingCode");
-ModelPath="../models/CatDogResNet.pth"
+ModelPath="../models/UrbanSoundsWithCats.pth"
 
-ModelData = torch.load(ModelPath)
+ModelData = torch.load(ModelPath,map_location='cpu')
 print(ModelData.keys())
 Input_Resolution = ModelData['resolution']
 ### Spectograph resolution
-SAMPLE_RATE = 22050
-N_FFT = 1024
-HOP_LENGTH = 256
-N_MELS = Input_Resolution
-FMIN = 20
-FMAX = SAMPLE_RATE / 2 
+SpectrumVariables = pickle.load(open( '../01_Spectrum Generation/test7.SpectrumVariables', "rb" ) )
 
+SAMPLE_RATE=SpectrumVariables["SAMPLE_RATE"]
+N_FFT=SpectrumVariables["N_FFT"]
+HOP_LENGTH= SpectrumVariables["HOP_LENGTH"]
+FMIN=SpectrumVariables["FMIN"]
+FMAX=SpectrumVariables["FMAX"]
+N_MELS=SpectrumVariables["N_MELS"]
+POWER=SpectrumVariables["POWER"]
+FMAX=SAMPLE_RATE/2
 ### Debug Info
 K=2
 
@@ -63,7 +67,7 @@ def imshow(img):
     plt.show()
 transform = transforms.Compose(
     [transforms.ToPILImage(),
-    transforms.Resize(224),
+     transforms.Resize(224),
      transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
@@ -95,7 +99,7 @@ running = True
 def callback(in_data, frame_count, time_info, flag):
     audio_data = np.frombuffer(in_data, dtype=np.float32)
     audio_data = librosa.resample(audio_data, 44100, SAMPLE_RATE)
-    ringBuffer.extend(audio_data)   
+    ringBuffer.extend(audio_data)
     return None, pyaudio.paContinue
 
 
@@ -108,23 +112,27 @@ def infere_Class_Type():
     audio_data = np.array(ringBuffer)
     mel_spec_power = librosa.feature.melspectrogram(audio_data, sr=SAMPLE_RATE, n_fft=N_FFT,
                                                 hop_length=HOP_LENGTH,
-                                                n_mels=N_MELS, power=2.0,
+                                                n_mels=N_MELS, power=POWER,
                                                fmin=FMIN,fmax=FMAX)
     mel_spec_db = librosa.power_to_db(mel_spec_power, ref=np.max)
-    
-    image=mel_spec_db[0:Input_Resolution,0:Input_Resolution]
 
-    im = plt.imshow(image)
-    colors = im.cmap(im.norm(image))
-    data = colors.astype(np.float64) / np.max(colors)
-    data = 255 * data 
-    img = data.astype(np.uint8)
-    img = img[:,:,0:3]
-    cv.imshow('dst_rt', img)
-    cv.waitKey(1)
-    if(mel_spec_db.shape[1]<Input_Resolution):
+    image=mel_spec_db[0:Input_Resolution,0:Input_Resolution]
+    image = mel_spec_db; # convert to float
+    image -= image.min() # ensure the minimal value is 0.0
+    image /= image.max() # maximum value in image is now 1.0
+    image*=256;
+    img = image.astype(np.uint8)
+    colerPic = cv2.applyColorMap(img, cv2.COLORMAP_BONE)
+    if( int(np.floor(colerPic.shape[1]/Input_Resolution))<0):
         return;
-    imagesTensor = transform(img)
+    #print(colerPic[:,-Input_Resolution:,:].shape)
+    OutputImage = cv2.resize(colerPic[:,-Input_Resolution:,:],(Input_Resolution,Input_Resolution))
+    cv2.imshow('dst_rt', OutputImage)
+    cv2.waitKey(1)
+
+    if(colerPic.shape[1]<Input_Resolution):
+        return;
+    imagesTensor = transform(colerPic)
     imagesTensor = Variable(imagesTensor, requires_grad=True)
     testImages = imagesTensor.unsqueeze(0)
     outputs = model(testImages)
@@ -133,10 +141,13 @@ def infere_Class_Type():
     predicted=predicted[0].numpy()
     prob=prob[0].detach().numpy()
     print('---')
-    for  j in range(K):
-        if prob[j] > 14.0:
-            print('Predicted:\t{} \t| Probablilty: \t{:f}'.format(classes[predicted[j]],prob[j]))
-    
+    for  j in range(len(predicted)):
+        #if prob[j] > 10.0 and len(predicted)>j+1 and len(prob)>j+1 :
+        if(predicted[j]>len(classes)):
+            print(predicted,len(classes))
+            continue
+        print('Predicted:\t{} \t| Probablilty: \t{:f}'.format(classes[predicted[j]],prob[j]))
+
 
 
 def startProgram(targetLength=20):
@@ -165,4 +176,3 @@ if __name__ == '__main__':
     time.sleep(1)
     pa.terminate()
     stream.close()
-
