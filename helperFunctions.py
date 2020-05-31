1a1e931c2ba89367c961115050ad5f285050dbc3
David Goedicke <da.goedicke@gmail.com>
2020-05-31 13:22:02 -0400

    ud

---
+++ helperFunctions.py
@@ -0,0 +1,100 @@
+from __future__ import print_function
+import csv
+import numpy as np
+import librosa
+import wave
+import os
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import pickle
+
+from ipywidgets import interactive
+import ipywidgets as widgets
+
+from PIL import Image
+import IPython.display as displayImg
+
+
+from ipywidgets import interact, widgets
+import glob
+import IPython.display as ipd
+
+import sys
+
+
+
+def listdir_nohidden(path):
+    return glob.glob(os.path.join(path, '*'))
+
+def GenerateSpectrums(MainFile):
+    SpectrumVariables={}
+    with open('../SpectrumVarialbes.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            for k in row:
+                SpectrumVariables[k]=int(row[k])
+
+    x ,sample_rate_in = librosa.load(MainFile,mono=True)
+    audio_data = librosa.resample(x, sample_rate_in, SpectrumVariables['SAMPLE_RATE'])
+    mel_spec_power = librosa.feature.melspectrogram(audio_data, sr=SpectrumVariables['SAMPLE_RATE'],
+                                                    n_fft=SpectrumVariables['N_FFT'],
+                                                    hop_length=SpectrumVariables['HOP_LENGTH'],
+                                                    n_mels=SpectrumVariables['N_MELS'],
+                                                    power=SpectrumVariables['POWER'],
+                                                   fmin=SpectrumVariables['FMIN'],
+                                                    fmax=SpectrumVariables['FMAX'])
+    mel_spec_db = np.float32(librosa.power_to_db(mel_spec_power, ref=np.max))
+    mel_spec_db-=mel_spec_db.min()
+    mel_spec_db/=mel_spec_db.max()
+    im = np.uint8(cm.gist_earth(mel_spec_db)*255)[:,:,:3]
+    ArrayofPictures = []
+    RESOLUTION = SpectrumVariables['RESOLUTION']
+    for i in range(int(np.floor(im.shape[1]/RESOLUTION))):
+        startx=RESOLUTION*i
+        stopx=RESOLUTION*(i+1)
+        ArrayofPictures.append(im[:,startx:stopx,:])
+    return ArrayofPictures
+
+def log_mel_spec_tfm(dataInput):
+    src_path=dataInput[0]
+    dst_path=dataInput[1]
+    #print(src_path, dst_path)
+    print('Starting on',os.path.split(src_path)[1])
+    pictures = GenerateSpectrums(src_path)
+    print(len(pictures))
+    fname = os.path.split(src_path)[-1]
+    count=0
+    for pic in pictures:
+        plt.imsave(os.path.join(dst_path,(fname.replace(".flac",'-')\
+                                          .replace(".aif",'-').replace(".wav",'-')\
+                                          .replace(".m4a",'-').replace(".mp3",'-')+str(count)+'.png')), pic)
+        count+=1
+    if(count==0):
+        print(src_path)
+
+        
+try:
+    Type
+except NameError:
+    if(len(sys.argv)>1):
+        print("FoundArguments, will start converting")
+        source = str(sys.argv[1])
+        target = str(sys.argv[2])
+        log_mel_spec_tfm((source,target))
+else:
+    if(Type=="INTERFACE"):
+        SOURCE_DATA_ROOT='../AudioData/'
+
+        style = {'description_width': 'initial'}
+
+        ClassSelection = widgets.Dropdown(options=listdir_nohidden(SOURCE_DATA_ROOT), description='Source for Training Data:',style=style)
+        FileSelection = widgets.Dropdown(description='Audio file to visualize',style=style)
+
+        def updateLocation(*args):
+            FileSelection.options=listdir_nohidden(os.path.join(SOURCE_DATA_ROOT,ClassSelection.value))
+
+        ClassSelection.observe(updateLocation)
+
+        display(ClassSelection)
+        display(FileSelection)
+        updateLocation();
